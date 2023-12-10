70a8afb6262014958da67a8fef055b4315c38f31
yui <crazyriver0224@gmail.com>
2023-12-10 23:19:04 +0800

    1

--- xmodem.c
+++ xmodem.c
@@ -7,7 +7,17 @@ struct xmodem_crc16_protocol_struct
     uint8_t header;
     uint8_t block_num;
     uint8_t inverse_block_num;
+    uint8_t *p_data;
     uint16_t crc16;
+};
+
+struct xmodem_checksum_protocol_struct
+{
+    uint8_t header;
+    uint8_t block_num;
+    uint8_t inverse_block_num;
+    uint8_t *p_data;
+    uint8_t checksum;
 };
 
 
